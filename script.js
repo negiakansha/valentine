373d5393375895200753d761d539679e6d0de961
Akansha Negi <akanshanegi@gmail.com>
2024-02-02 20:13:52 -0600

    update buttons

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const TOGGLE = document.querySelector('button');
 const themeSwitch = document.getElementById('themeSwitch');
 const root = document.documentElement;
+const toggleButton = document.getElementById('toggleButton');
+const toggleButton2 = document.getElementById('toggleButton2');
 
 // Function to toggle light and dark themes
 function toggleTheme() {
-	if (!themeSwitch.checked) {
+	if (themeSwitch.checked) {
 		root.style.setProperty('--bg-color', '#FFE3EC'); // Light mode background color
-		root.style.setProperty('--text-color', 'black');  // Light mode text color
+		root.style.setProperty('--text-color', '#e75480');  // Light mode text color
+		root.style.setProperty('--button-color', '#e75480');  // Light mode button text color
+		toggleButton.classList.remove('btn-6');
+        toggleButton.classList.add('btn-7');
+		toggleButton2.classList.remove('btn-6');
+        toggleButton2.classList.add('btn-7');
 		if (themeSwitch.nextElementSibling) {
 			themeSwitch.nextElementSibling.textContent = 'Light Mode';
 		}
 	} else {
 		root.style.setProperty('--bg-color', '#e75480');  // Dark mode background color
-		root.style.setProperty('--text-color', 'white'); // Dark mode text color
+		root.style.setProperty('--text-color', '#FFE3EC'); // Dark mode text color
+		root.style.setProperty('--button-color', '#FFE3EC');  // Light mode button text color
+		toggleButton.classList.remove('btn-7');
+        toggleButton.classList.add('btn-6');
+		toggleButton2.classList.remove('btn-7');
+        toggleButton2.classList.add('btn-6');
 		if (themeSwitch.nextElementSibling) {
 			themeSwitch.nextElementSibling.textContent = 'Dark Mode';
 		}
